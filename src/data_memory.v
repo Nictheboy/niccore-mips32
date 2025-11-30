cdf517a9c79c110fd7513de95b771c5062de6924
nictheboy <nictheboy@outlook.com>
2025-12-01 01:45:05 +0800

    feat: resize data memory to 8 KB

--- src/data_memory.v
+++ src/data_memory.v
@@ -14,23 +14,30 @@ module data_memory (
     input [31:0] write_input,
     output reg [31:0] read_result
 );
-    localparam UNIT_COUNT = 1024;
-    localparam VALID_ADDRESS_WIDTH = 10;
+    localparam UNIT_COUNT = 2048;
+    localparam VALID_ADDRESS_WIDTH = 11;
 
     reg [31:0] data[UNIT_COUNT-1 : 0];
     wire [VALID_ADDRESS_WIDTH-1 : 0] valid_address = address[VALID_ADDRESS_WIDTH+2-1 : 2];
-    wire address_is_valid = (address == valid_address);
+    wire address_is_valid = (address == {{(30 - VALID_ADDRESS_WIDTH) {1'b0}}, valid_address});
+
+    always @(*) begin
+        if (address_is_valid) begin
+            read_result = data[valid_address];
+        end else begin
+            read_result = 32'hxxxxxxxx;
+        end
+    end
 
     integer i;
-    always @(posedge clock) begin
+    always @(negedge clock) begin
         if (reset) begin
-            for (i = 0; i < UNIT_COUNT; i = i + 1) data[i] <= 32'h00000000;
-            read_result <= 32'hxxxxxxxx;
-        end else if (address_is_valid) begin
-            if (write_enable) begin
-                data[valid_address] <= write_input;
-                read_result <= 32'hxxxxxxxx;
-            end else read_result <= data[valid_address];
-        end else read_result <= 32'hxxxxxxxx;
+            for (i = 0; i < UNIT_COUNT; i = i + 1) begin
+                data[i] <= 32'h00000000;
+            end
+        end else if (write_enable && address_is_valid) begin
+            data[valid_address] <= write_input;
+        end
     end
+
 endmodule
